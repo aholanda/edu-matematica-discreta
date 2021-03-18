2bf5a8dc4c45bc584d22d6c256977f70503dfa84
Adriano J. Holanda <adriano.holanda@fafram.com.br>
2021-03-18 14:39:41 -0300

    setup transferred to Makefile

--- notebooks/setup.sh
+++
@@ -1,9 +0,0 @@
- 2002  jupyter-nbextension install rise --py --sys-prefix
- 2003  sudo jupyter-nbextension install rise --py --sys-prefix
- 2004  sudo pip install jupyter
- 2005  sudo jupyter-nbextension install rise --py --sys-prefix
- 2006  sudo jupyter-nbextension install RISE --py --sys-prefix
- 2008  sudo jupyter-nbextension install RISE --py --sys-prefix
- 2009  sudo jupyter-nbextension install rise --py --sys-prefix
- 2010  sudo jupyter-nbextension install rise --py 
- 2031  history|grep jupy
